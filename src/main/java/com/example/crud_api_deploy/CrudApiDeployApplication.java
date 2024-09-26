bc092f0a1a2d07e2d4ad59fe24b100518a15c2a8
Gursharan Singh <gagan93singh@gmail.com>
2024-09-27 02:37:38 +0530

    Initial Commit

---
+++ src/main/java/com/example/crud_api_deploy/CrudApiDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_api_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crud_api_deploy/CrudApiDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_api_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiDeployApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
